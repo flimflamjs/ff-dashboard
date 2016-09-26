03b2c6f22da8b53dacbca00e6ff5e0b1e809ffb8
yutaka_houlette <yutakahoulette@gmail.com>
2016-09-26 13:37:10 -0700

    Format some more data

--- example/main.js
+++ example/main.js
@@ -2,12 +2,24 @@ import R from 'ramda'
 import h from 'snabbdom/h'
 import data from './data'
 
+
 const row = state => d =>
-  h('tr', [
+  h('tr.small', [
     h('td', [h('a', {on: {click: x => state.dataId$(d.id)}}, d.name)])
-  , h('td', d.description.substring(0, 70) + '...')
+  , h('td', d.year) 
+  , h('td', Number(d.length).toFixed(2).replace('.', ':'))
+  , h('td', R.map(x => x.name, d.personnel).join(', '))
   ])
 
 module.exports = state => 
-  h('table', R.map(row(state), data))
+  h('table.fullWidth', R.concat(
+    [
+      h('tr.bold.small', [
+        h('td', 'Name')
+      , h('td', 'Year')
+      , h('td', 'Length')
+      , h('td', 'Musicians')
+      ])
+    ], R.map(row(state), data))
+  )
 
