7a2c6b3796440acdd469b45e7df34b1b5d05d70e
yutaka_houlette <yutakahoulette@gmail.com>
2016-10-04 17:43:26 -0700

    Get filtering to work

--- example/filter.js
+++ example/filter.js
@@ -3,26 +3,27 @@ import flyd from 'flyd'
 import R from 'ramda'
 import data from './data'
 
-const input = (type, name) => value =>
-  h('div.small', [
-    h('input', {props: {type, name, value}})
-  , h('span.pl1', value)
-  ])
 
-const checkboxes = (arr, name) =>
-  h('div', R.map(x => input('checkbox', name)(x), arr))
+const checkboxes = (state, arr, name) =>
+  h('form', {on: {change: state.filterInput$}}
+  , R.flatten(
+      R.map(x => [
+        h('input', {props: {type: 'checkbox', name, value: x}})
+      , h('span.pl1', x)
+      , h('br')
+      ], arr)
+    )
+  )
   
 const getPersonnel = _ => 
     R.uniq(R.reduce((a, b) => R.concat(a, R.map(x => x.name, b.personnel)), [], data))
 
-
 module.exports = state => {
-  // let instruments = R.map(x => R.concat(R.map(y => y.instruments, x.personnel)), data)
   let personnel = getPersonnel()
 
   return h('div', [
     h('p.bold.mt0.mb1', 'Personnel')
-  , checkboxes(personnel, 'personnel')
+  , checkboxes(state, personnel, 'personnel')
   ])
 }
 
