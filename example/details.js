516d46d5c032bfa25e07d4ecf54aefbecc1677ab
yutaka_houlette <yutakahoulette@gmail.com>
2016-11-03 11:14:00 -0700

    Finish updating example

--- example/details.js
+++ example/details.js
@@ -11,23 +11,27 @@ const personnel = state => p =>
   )
 
 module.exports = state => 
-  h('div.pb2', [
-    h('div.table', [
-      h('img.table-cell.align-middle', {props: {src: state.dataDetails$().img}})
-    , h('table.small.table-cell.align-middle.pl1', [
-        h('tr', [ h('td.bold', 'Year') , h('td', state.dataDetails$().year) ])
-      , h('tr', [ h('td.bold', 'Length')
-        , h('td', Number(state.dataDetails$().length).toFixed(2).replace('.', ':')) ])
-      , h('tr', [ 
-          h('td.bold', 'Label')
-        , h('td', R.map(x => x, state.dataDetails$().label || []).join(', '))
+  h('div', [
+    h('h4.p-2.m-0.line-height-1', state.dataDetails$().name)
+  , h('hr.m-0')
+  , h('div.p-2', [
+      h('div.table', [
+        h('img.table-cell.align-middle', {props: {src: state.dataDetails$().img}})
+      , h('table.table-cell.align-middle.pl-1', [
+          h('tr', [ h('td.bold', 'Year') , h('td', state.dataDetails$().year) ])
+        , h('tr', [ h('td.bold', 'Length')
+          , h('td', Number(state.dataDetails$().length).toFixed(2).replace('.', ':')) ])
+        , h('tr', [ 
+            h('td.bold', 'Label')
+          , h('td', R.map(x => x, state.dataDetails$().label || []).join(', '))
+          ])
         ])
       ])
+    , h('p.mt-2', state.dataDetails$().blurb)
+    , h('h4.mb-1', 'Tracks')
+    , h('ol', R.map(x => h('li', x), state.dataDetails$().tracks || []))
+    , h('h4.mb-1', 'Personnel')
+    , h('table', R.map(personnel(state), state.dataDetails$().personnel || []))
     ])
-  , h('p.mt2', state.dataDetails$().blurb)
-  , h('h4.mb1', 'Tracks')
-  , h('ol.small', R.map(x => h('li', x), state.dataDetails$().tracks || []))
-  , h('h4.mb1', 'Musicians')
-  , h('table.small', R.map(personnel(state), state.dataDetails$().personnel || []))
   ])
 
